d621a4884bd2e419b9768095a6a9fb353cb6483e
enRamirez <ricky.ramirez125042@gmail.com>
2023-07-17 13:02:05 -0500

    Started on main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class bankAccount {
+    constructor(accountNumber, owner){
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.transaction = [];
+    }
+}
